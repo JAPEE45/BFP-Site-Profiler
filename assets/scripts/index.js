8cde5dcf474cde0ace24fd18e33a6fe6a6d30c12
chris <chrisbriollovehannah@gmail.com>
2025-09-07 21:52:41 +0800

    admin semi done

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -14,15 +14,25 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
     // Simulate login process (replace with actual login logic)
     setTimeout(() => {
       button.classList.remove("loading");
-      button.innerHTML = '<i class="fas fa-check me-2"></i>Success!';
+      
+
+      
+        if (username === "admin" && password === "admin") {
+          button.innerHTML = '<i class="fas fa-check me-2"></i>Success!';
       button.style.background =
         "linear-gradient(135deg, #28a745 0%, #1e7e34 100%)";
-
-      // Reset button after 2 seconds
-      setTimeout(() => {
-        button.innerHTML = '<i class="fas fa-sign-in-alt me-2"></i>Login';
-        button.style.background = "";
-      }, 2000);
+          window.location.href = "./admin/dashboard.html";
+        } else {
+          button.innerHTML = '<i class="fa-solid fa-xmark me-2"></i>Login Failed!';
+      button.style.background =
+        "linear-gradient(135deg, red 0%, red 100%)";
+          alert("Invalid username or password!");
+          setTimeout(() => {
+            button.innerHTML = '<i class="fas fa-sign-in-alt me-2"></i>Login';
+            button.style.background =
+              "linear-gradient(135deg, var(--bfp-red) 0%, var(--bfp-dark-red) 100%)";
+          }, 2000)
+        }
     }, 1500);
   }
 });
